953103d232fcfda916b704e70d16e3d27299ede6
Jim Newpower <jimnewpower@gmail.com>
2024-01-10 16:43:03 -0700

    Use Executor service to process each batch job asynchronously

--- homegrown/src/main/java/com/example/homegrown/BatchJob.java
+++ homegrown/src/main/java/com/example/homegrown/BatchJob.java
@@ -2,17 +2,24 @@ package com.example.homegrown;
 
 import java.io.PrintStream;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A batch job that will read a file and batch process the data.
  */
 class BatchJob<T> implements Runnable {
 
+    private final ExecutorService executorService;
+
     private final int maxTasksToQueue;
     private final ResourceReader<T> resourceReader;
     private final PrintStream printStream;
     
-    BatchJob(int maxTasksToQueue, ResourceReader<T> resourceReader, PrintStream printStream) {
+    BatchJob(ExecutorService executorService, int maxTasksToQueue, ResourceReader<T> resourceReader, PrintStream printStream) {
+        this.executorService = Objects.requireNonNull(executorService, "executor service");
         this.maxTasksToQueue = Math.max(1, maxTasksToQueue);
         this.resourceReader = Objects.requireNonNull(resourceReader, "resource reader");
         this.printStream = Objects.requireNonNull(printStream, "print stream");
@@ -24,12 +31,14 @@ class BatchJob<T> implements Runnable {
         printStream.println("Processing batch job...");
         printStream.println();
 
+        Set<Future<?>> futures = new HashSet<>();
+
         // create the batcher, including the consumer that will process the data
         Batcher<T> batcher = new Batcher<>(maxTasksToQueue, data -> {
             // process the chunk of data for a single batch job
             List<T> content = new ArrayList<>(data);
-            printStream.println("** Batch Operation " + UUID.randomUUID());
-            content.forEach(printStream::println);
+//            batchTask(content).run();
+            futures.add(executorService.submit(batchTask(content)));
         });
 
         // read the data and submit to the batcher
@@ -43,6 +52,29 @@ class BatchJob<T> implements Runnable {
             ;
         }
 
+        // wait for all tasks to finish
+        futures.forEach(future -> {
+            try {
+                while (!future.isDone()) {
+                    System.out.println("Waiting for task to complete...");
+                    future.get(1, TimeUnit.SECONDS); // Wait for 1 second before checking again
+                }
+                System.out.println("Task is done: " + future.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
         printStream.println("Elapsed time for batch job: " + (System.currentTimeMillis() - start));
+
+        System.out.println("Done");
+    }
+
+    private Runnable batchTask(List<T> data) {
+        return () -> {
+            // process the chunk of data for a single batch job
+            printStream.println("** Batch Operation " + UUID.randomUUID());
+            data.forEach(printStream::println);
+        };
     }
 }
--- homegrown/src/main/java/com/example/homegrown/HomegrownApplication.java
+++ homegrown/src/main/java/com/example/homegrown/HomegrownApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class HomegrownApplication implements CommandLineRunner {
 	private static final String TRANSACTIONS_FILENAME = "mock_transactions.csv";
@@ -14,9 +18,24 @@ public class HomegrownApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
 		ResourceReader<String> resourceReader = new ResourceReaderText(TRANSACTIONS_FILENAME);
 		final int maxTasksToQueue = 100;
-		BatchJob<String> batchJob = new BatchJob<>(maxTasksToQueue, resourceReader, System.out);
+		BatchJob<String> batchJob = new BatchJob<>(executorService, maxTasksToQueue, resourceReader, System.out);
 		batchJob.run();
+		cleanup(executorService);
 	}
+
+	private void cleanup(ExecutorService executorService) {
+		executorService.shutdown();
+		try {
+			Thread.sleep(1000);
+			if (!executorService.awaitTermination(2000, TimeUnit.MILLISECONDS)) {
+				executorService.shutdownNow();
+			}
+		} catch (InterruptedException e) {
+			executorService.shutdownNow();
+		}
+	}
+
 }
